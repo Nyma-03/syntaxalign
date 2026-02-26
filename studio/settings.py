41f1f8722c2e2529c46d684f2de9186c1db1c5b0
Nyma <meemtabassum246@gmail.com>
2026-02-26 16:58:44 +0600

    Switch database to SQLite for Render deployment

--- studio/settings.py
+++ studio/settings.py
@@ -90,18 +90,23 @@ WSGI_APPLICATION = "studio.wsgi.application"
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'studio_db',
+#         'USER': 'root',
+#         'PASSWORD': 'meem',
+#         'HOST': 'localhost',
+#         'PORT': '3307',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'studio_db',
-        'USER': 'root',
-        'PASSWORD': 'meem',
-        'HOST': 'localhost',
-        'PORT': '3307',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
